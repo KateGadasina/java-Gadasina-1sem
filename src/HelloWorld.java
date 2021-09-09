89ac4b4c8e6973f2714bc9d35b970fcb7b4360e7
79218 <k.gadasina@yandex.ru>
2021-09-09 12:47:08 +0300

    решение новой задачи

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args){
-        // изменения в тексте
+        // решение новой задачи
         System.out.println("Hello World! 123");
     }
 }
