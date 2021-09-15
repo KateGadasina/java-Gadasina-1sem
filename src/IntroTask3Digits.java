3655cf14886ae3e77358df79305b9d98f2649ee7
79218 <k.gadasina@yandex.ru>
2021-09-16 00:31:00 +0300

    решение задачи IntroTask3Digits

---
+++ src/IntroTask3Digits.java
@@ -0,0 +1,9 @@
+public class IntroTask3Digits {
+    public static void main(String[] args){
+        int x = 2390;
+        if (x / 100 > 0 && x / 100 < 10)
+            System.out.println(true);
+        else
+            System.out.println(false);
+    }
+}
