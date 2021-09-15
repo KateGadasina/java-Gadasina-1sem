7720a1d4ad51b1392b86163c865e59d69d283e2d
79218 <k.gadasina@yandex.ru>
2021-09-16 00:35:12 +0300

    решение задачи IntroTaskLast5

---
+++ src/IntroTaskLast5.java
@@ -0,0 +1,9 @@
+public class IntroTaskLast5 {
+    public static void main(String[] args){
+        int x = 2395;
+        if (x % 10 == 5)
+            System.out.println(true);
+        else
+            System.out.println(false);
+    }
+}
